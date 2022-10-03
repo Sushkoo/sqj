2387b8ed855513557b94a20aad24f557e3efb94a
Sushi <70635877+Sushkoo@users.noreply.github.com>
2022-10-03 13:47:26 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -8,10 +8,10 @@ CREATE TABLE barlang
     melyseg DOUBLE(6,1),
     magassag DOUBLE(6,1),
     telepules VARCHAR(50)
-)
+);
 
 DELETE FROM barlang
-where telepules="Tés"
+where telepules="Tés";
 
 INSERT INTO barlang values("Új barlang 1",200,10,30,10,"XYZ");
 
@@ -19,19 +19,19 @@ INSERT INTO barlang values("Új barlang",150,20,30,10,"");
 
 UPDATE barlang
 SET hossz=hossz+1
-WHERE nev="Duó-zsomboly"
+WHERE nev="Duó-zsomboly";
 
 SELECT nev FROM barlang
-WHERE telepules="Budapest"
+WHERE telepules="Budapest";
 
 SELECT NEV FROM barlang
-WHERE hossz>=1000 AND magassag<50
+WHERE hossz>=1000 AND magassag<50;
 
 SELECT * FROM barlang
-WHERE hossz<100 AND nev LIKE "%zsomboly%"
+WHERE hossz<100 AND nev LIKE "%zsomboly%";
 
 SELECT nev, melyseg, telepules FROM barlang
-WHERE melyseg>=50 AND melyseg<=100
+WHERE melyseg>=50 AND melyseg<=100;
 
 SELECT nev from barlang
-WHERE nev LIKE "%zsomboly%"
+WHERE nev LIKE "%zsomboly%";
